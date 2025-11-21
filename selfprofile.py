944dbca4e199734966ba08b3216987f61e1fb11f
azrapallan <pallanazra@gmail.com>
2025-11-21 11:53:15 +0530

    Update selfprofile.py

--- selfprofile.py
+++ selfprofile.py
@@ -5,7 +5,6 @@ args = sys.argv[1:]
 if len(args) >= 3:
     hobbies, strength, weakness = args[:3]
 else:
-    print("Using default profile values — not enough arguments.\n")
     hobbies = "implementing skills"
     strength = "coding skills"
     weakness = "weak in debugging"
@@ -14,3 +13,4 @@ print("hobbies:", hobbies)
 print("strength:", strength)
 print("weakness:", weakness)
 print("self_profile:", (hobbies, strength, weakness))
+
