6aa8df0fec93d294b6bb213ade272cfb6b0c536d
azrapallan <pallanazra@gmail.com>
2025-11-21 10:56:46 +0530

    Update selfprofile.py

---
+++ selfprofile.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv) > 3:
+    hobbies = sys.argv[1]
+    strength = sys.argv[2]
+    weakness = sys.argv[3]
+else:
+    hobbies = "implementing skills"
+    strength = "coding skills"
+    weakness = "weak in debugging"
+self_profile = (hobbies, strength, weakness)
+print("hobbies:", hobbies)
+print("strength:", strength)
+print("weakness:", weakness)
+print("self_profile:", self_profile)
